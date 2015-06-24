a71136ad84a6b2d0fef76a367ea4579c426936bb
AL01F13 <fernanda.aguilarj@gmail.com>
2015-06-24 18:29:57 -0500

    uno
    violencia

---
+++ js/acciones.js
@@ -0,0 +1,30 @@
+// JavaScript Document
+$(document).ready(function(e) {
+document.addEventListener("deviceready",function(){
+	
+	$('#btnprincipal').on('tap',function(){
+	navigator.vibrate(1000);
+	});
+	
+	$('#btntipos').on('tap',function(){
+	navigator.vibrate(1000);
+	});
+	
+	$('#btnfactores').on('tap',function(){
+	navigator.vibrate(1000);
+	});
+	
+	$('#btntest').on('tap',function(){
+	navigator.vibrate(1000);
+	});
+	
+	$('#btngaleria').on('tap',function(){
+	navigator.vibrate(1000);
+	});
+	
+	$('#btnvideo').on('tap',function(){
+	navigator.vibrate(1000);
+	});
+}); 
+});
+
